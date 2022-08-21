5a1c51efe1553a9f7ad890c90c17aea4e36a0a39
Will Cullen <itzcull@outlook.com>
2022-08-21 12:56:09 +1000

    CssBaseline, blank app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
 import React from "react"
-import '../styles/globals.css'
 import { AppProps } from "next/app"
+import { CssBaseline } from "@mui/material"
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (<>
+    <CssBaseline />
+    <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
